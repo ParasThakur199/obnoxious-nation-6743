4f2b4ff71c73d841fdcb342b7c6089423c4536ed
ParasThakur199 <parasjamwal19@gmail.com>
2023-01-19 18:13:13 +0530

    add day-3

--- Student Registratiion System/src/com/Utility/DButil.java
+++ Student Registratiion System/src/com/Utility/DButil.java
@@ -1,7 +1,9 @@
-package comUtility;
+package com.Utility;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+
 
 public class DButil {
 	public static Connection getConnection() throws SQLException{
@@ -12,8 +14,13 @@ public class DButil {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		return null;
+		String url = "jdbc:mysql://localhost:3306/student_management_system";
+		try {
+			conn = DriverManager.getConnection(url,"root","root");
+		} catch (SQLException e) {
+			throw new SQLException(e.getMessage());
+		}
+		return conn;
 		
 	}
 }
