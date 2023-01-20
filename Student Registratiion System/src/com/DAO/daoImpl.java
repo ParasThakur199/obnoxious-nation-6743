b1bfe07fcd051cb6ee534edbe31522ec641fd7f1
ParasThakur199 <parasjamwal19@gmail.com>
2023-01-20 12:36:46 +0530

    add changes

--- Student Registratiion System/src/com/DAO/daoImpl.java
+++ Student Registratiion System/src/com/DAO/daoImpl.java
@@ -1,5 +1,37 @@
 package com.DAO;
 
-public class daoImpl {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.Model.Student;
+import com.Utility.DButil;
+
+public class daoImpl implements dao{
+
+	@Override
+	public String registerStudent(Student student) throws SQLException {
+		
+		String answer = "Student Already Exists !!";
+		
+		String StdEmail = student.getStdEmail();
+		String StdPassword = student.getStdPassword();
+		String StdName = student.getStdName();
+		
+		
+		try(Connection conn = DButil.getConnection()) {
+			PreparedStatement ps = conn.prepareStatement("Insert into students values(?,?,?)");
+			ps.setString(1,StdEmail);
+			ps.setString(2,StdPassword);
+			ps.setString(3, StdName);
+			
+			int x = ps.executeUpdate();
+			answer = x + "Student Statements Inserted";
+
+		} catch (SQLException e) {
+			throw new SQLException(e.getMessage());
+		}
+		return answer;
+	}
 
 }
--- Student Registratiion System/src/com/Model/Student.java
+++ Student Registratiion System/src/com/Model/Student.java
@@ -1,5 +1,51 @@
 package com.Model;
 
 public class Student {
+	private String StdEmail;
+	private String StdPassword;
+	private String StdName;
+	
+	public Student() {
+		
+	}
 
+	public Student(String stdEmail, String stdPassword, String stdName) {
+		super();
+		StdEmail = stdEmail;
+		StdPassword = stdPassword;
+		StdName = stdName;
+	}
+
+	public String getStdEmail() {
+		return StdEmail;
+	}
+
+	public void setStdEmail(String stdEmail) {
+		StdEmail = stdEmail;
+	}
+
+	public String getStdPassword() {
+		return StdPassword;
+	}
+
+	public void setStdPassword(String stdPassword) {
+		StdPassword = stdPassword;
+	}
+
+	public String getStdName() {
+		return StdName;
+	}
+
+	public void setStdName(String stdName) {
+		StdName = stdName;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [StdEmail=" + StdEmail + ", StdPassword=" + StdPassword + ", StdName=" + StdName + "]";
+	}
+	
+	
+	
+	
 }
