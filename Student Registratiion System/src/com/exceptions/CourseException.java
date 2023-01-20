ea7c4ca150d07f229a39b5ec6290c68a92196b8f
ParasThakur199 <parasjamwal19@gmail.com>
2023-01-20 21:55:26 +0530

    some changes

---
+++ Student Registratiion System/src/com/exceptions/CourseException.java
@@ -0,0 +1,12 @@
+package com.exceptions;
+
+public class CourseException extends Exception{
+	
+	public CourseException() {
+		
+	}
+	
+	public CourseException(String message) {
+		super(message);
+	}
+}
